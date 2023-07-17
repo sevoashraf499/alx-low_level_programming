ac62136b175c5631d250bca961fb1e3e9048314d
sevoashraf499 <sevoashraf499@gmail.com>
2023-07-16 21:53:39 -0500

    update task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - prints the lowercase alphabet in reverse,
- * followed by a new line
+ * main - Entry point
+ *
+ * Description: Prints all letters from z to a.
+ * starting from 0 to 9
+ *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char ch;
+	char c;
 
-	for (ch = 'z'; ch >= 'a'; ch--)
-	{
-		putchar(ch);
-	}
+	for (c = 'z'; c > 96; c--)
+		putchar(c);
+
 	putchar('\n');
+
 	return (0);
 }
