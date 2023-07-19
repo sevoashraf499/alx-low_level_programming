442ff57bbee5637411b01f04c4780e6a8c63a72f
sevoashraf499 <sevoashraf499@gmail.com>
2023-07-19 07:36:22 -0500

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,8 +5,15 @@
  * @n: the int to extract the last digit from
  * Return: value of the last digit
  */
+
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int r;
+
+	r = n % 10;
+
+	_putchar(r + '0');
+
+	return (r);
 }
 
