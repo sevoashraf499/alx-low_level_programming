ee7a29c0db40ba3d490670a089caab0c64b0305a
sevoashraf499 <sevoashraf499@gmail.com>
2023-08-04 14:44:50 -0500

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,6 +17,8 @@ void print_to_98(int n)
 			printf("%d, ", i);
 		else
 			printf("%d", i);
+
+		return (0);
 	}
 
 	for (i = n; i >= 98; i--)
@@ -25,6 +27,8 @@ void print_to_98(int n)
 			printf("%d, ", i);
 		else
 			printf("%d", i);
+
+		return (0);
 	}
 }
 
