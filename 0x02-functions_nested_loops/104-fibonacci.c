50f6813d9df8a743101437a2a09e4cbb115123d0
sevoashraf499 <sevoashraf499@gmail.com>
2023-08-05 04:05:54 -0500

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,48 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - fibonacci <3
- *
- * Purpose - no hardcode
- *
- * Return:  (Success)
+ * main - finds and prints the sum of the even-valued terms
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
+    int i;
+	unsigned long fib1 = 0, fib2 = 1, sum = 0;
 
-	printf("%lu", bef);
-
-	for (i = 1; i < 91; i++)
+	for (i = 0; i < 98; i++)
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
+		sum = fib1 + fib2;
+
+        if (i != 97)
+            printf("%lu, ", sum);
+        else
+            printf("%lu\n", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
 	}
 
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
-
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
-	}
-	printf("\n");
 	return (0);
 }
 
