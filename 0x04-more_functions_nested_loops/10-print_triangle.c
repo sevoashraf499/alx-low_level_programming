b2e21779039fa60951bd84773958560e00801359
sevoashraf499 <sevoashraf499@gmail.com>
2023-08-14 03:12:43 -0500

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,25 +10,20 @@
 
 void print_triangle(int size)
 {
-	int row, hashes, spaces;
+	int row, spaces, hashes;
 
 	if (size <= 0)
+		putchar('\n');
+
+	for (row = 1; row <= size; row++)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (row = 1; row <= size; row++)
-		{
-			for (spaces = size - row; spaces >= 1; spaces--)
-			{
-				_putchar(' ');
-			}
-			for (hashes = 1; hashes <= row; hashes++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
+		for (spaces = 0; spaces < size - row; spaces++)
+			putchar(' ');
+
+		for (hashes = 0; hashes < row; hashes++)
+			putchar('#');
+
+		putchar('\n');
 	}
 }
+
